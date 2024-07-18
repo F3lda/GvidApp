c16cafac3681a9e983fb1659e1e0d386413a2622
F3lda <kajel.jirgl@gmail.com>
2024-07-18 17:23:14 +0200

    Migration to AndroidX

--- app/src/main/java/cz/gvid/app/NonSwipeableViewPager.java
+++ app/src/main/java/cz/gvid/app/NonSwipeableViewPager.java
@@ -1,9 +1,10 @@
 package cz.gvid.app;
 
 import android.content.Context;
-import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+
+import androidx.viewpager.widget.ViewPager;
 
 public class NonSwipeableViewPager extends ViewPager {
 
